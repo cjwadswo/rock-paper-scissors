1dd34c650c15ab86931fd6f2e811a353f9eb822f
Chris Wadsworth <cjwadswo@uno.edu>
2022-04-15 02:57:07 -0500

    Add a best of 9 feature

--- rps.js
+++ rps.js
@@ -2,18 +2,18 @@
 function getPlayerChoice(){
     let choice = parseInt(prompt("Press 1 for Rock\nPress 2 for paper\nPress 3 for Scissors"));
     if( !(choice > 0 && choice < 4) ){
-        console.log(choice);
+      //  console.log(choice);
         alert("You entered an invalid choice. Click okay to try again");
         getPlayerChoice();
     }
-    console.log("You have choosen "+ choice);
+    //console.log("You have choosen "+ choice);
     return choice;
 }
 
 //Get computer's choice
 function getComputerChoice(){
     let choice = Math.floor((Math.random()*3)) + 1;
-    console.log("The computer has choosen " + choice);
+    //console.log("The computer has choosen " + choice);
     return choice;
 }
 
@@ -27,15 +27,15 @@ function computerWinner(playerChoice, computerChoice){
         //Check to see if the computer also chose Rock and console.log that it was tie
         if(computerChoice == 1){
 
-            console.log("Its a tie!");
+            return 0;
         }
         //Check to see if the computer chose Paper and console.log that the computer won. 
         else if( computerChoice == 2){
-            console.log("The computer won!");
+            return -1;
         }
         //Check is the computer chose Scissors and console.log that the user has won.
         else{
-            console.log("You have won!");
+            return 1;
         }
     } 
     //If player chose Paper
@@ -43,15 +43,15 @@ function computerWinner(playerChoice, computerChoice){
         //Check to see if the computer chose Rock the player won
         if(computerChoice == 1){
 
-            console.log("You have won!");
+            return 1;
         }
         //Check to see if the computer chose Paper and console.log that it was a tie
         else if( computerChoice == 2){
-            console.log("Its a tie!");
+            return 0;
         }
         //Check is the computer chose Scissors and console.log that the user has won.
         else{
-            console.log("The computer won!");
+            return -1;
         }
     }
     //Else the player had choosen scissors
@@ -59,23 +59,45 @@ function computerWinner(playerChoice, computerChoice){
         //Check to see if the computer has Rock and console.log that the computer has won
         if(computerChoice == 1){
 
-            console.log("The computer won!");
+            return -1;
         }
         //Check to see if the computer chose Paper and console.log that the user has won.
         else if( computerChoice == 2){
-            console.log("You won!");
+            return 1;
         }
         //Check is the computer chose Scissors and console.log that it was a tie.
         else{
-            console.log("Its a tie!");
+            return 0;
         }
     }
 
 }
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
-computerWinner(playerChoice,computerChoice);
+function play(){
+    //Best of 7
+    let playerRoundsWon = 0;
+    let computerRoundsWon = 0;
+    let winner;
+    while(playerRoundsWon < 5 && computerRoundsWon < 5){
+        winner = computerWinner(getPlayerChoice(), getComputerChoice());
+        console.log(winner);
+        if ( winner == 1 ) {
+            playerRoundsWon++;
+        }
+        else if (winner == -1) {
+            computerRoundsWon++;
+        }
+        else {
+            //tie
+        }
+        
+        console.log("Player: "+ playerRoundsWon + " Computer: " +computerRoundsWon);
+    }
+
+    
+}
+
+play();
 
 
 
